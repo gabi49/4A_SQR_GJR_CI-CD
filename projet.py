651ee3b230e0b3516d464c3c3dd75064a9f6e0df
ROMEO KOULARAMBAYE MBAINGUINAM <49437899+ROMEO2023@users.noreply.github.com>
2023-02-01 16:09:19 +0100

    Update projet.py

--- projet.py
+++ projet.py
@@ -87,6 +87,14 @@ def personBalance ():
 	
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    if len(sys.argv) > 1:
+        if sys.argv[1] == "check_syntax":
+            print("Build [ OK ]")
+            exit(0)
+        else:
+             print("Passed argument not supported ! Supported argument: check_syntax")
+             exit(1)
+
+    app.run(debug=True)
 
 
